4c1fa6f77424f7b883902dd52e262c4934605982
evan-bertis-sample <esample21@gmail.com>
2024-12-18 16:05:36 -0500

    some kind of build system

--- dbuild.sh
+++ dbuild.sh
@@ -2,7 +2,7 @@
 
 PROJECT_NAME=$1
 if [ -z "$PROJECT_NAME" ]; then
-    echo "Usage: $0 <PROJECT_NAME> [platform_name]"
+    echo "Usage: $0 <PROJECT_NAME> [platform_name] [project_dir]"
     exit 1
 fi
 
@@ -19,7 +19,7 @@ if [ -z "$PROJECT_DIR" ]; then
     echo "No project directory specified, using default project directory: ${PROJECT_DIR}"
 fi
 
-echo "Building example: ${PROJECT_NAME} for platform: ${PLATFORM_NAME}"
+echo "Building project: ${PROJECT_NAME} for platform: ${PLATFORM_NAME}"
 echo "-----------------------------------"
 echo ""
 
@@ -32,7 +32,20 @@ cmake -G "Unix Makefiles" -S . -B "${BUILD_DIR}" \
     -DPROJECT="${PROJECT_NAME}" \
     -DPROJECT_CMAKE_DIR="${PROJECT_DIR}"
 
-# Build only the requested example target from the specified build directory
+# Build only the requested project target from the specified build directory
 cmake --build "${BUILD_DIR}" --target "${PROJECT_NAME}"
 
-echo "Build complete. The executable should be in ${BUILD_DIR}/examples/${PROJECT_NAME}/"
+# now actually run make on the build directory
+cd "${BUILD_DIR}"
+make 
+
+echo "Build complete. The executable should be in ${BUILD_DIR}"
+
+
+# now run the executable
+echo "Running the executable..."
+echo "-----------------------------------"
+echo ""
+
+# Run the executable
+"./${BUILD_DIR}/${PROJECT_NAME}.exe"
---
+++ examples/blank/blank.cmake
@@ -0,0 +1,23 @@
+# This will be called from the top-level doodle CMakeLists.txt
+# it will be called using "include" and will be provided:
+# PLATFORM_NAME
+# PROJECT_NAME
+# PROJECT_CMAKE_DIR
+# PROJECT_SOURCE_DIR
+
+# this will have a function build_project that will be called
+
+function(build_project)
+    message(STATUS "Building blank example")
+    # find all of the .c files in this directory,
+    # they will be compiled into the blank executable
+
+    message(STATUS "Adding blank.c to the project, using path ${DOODLE_PROJECT_ROOT_DIR}/blank.c")
+
+    add_executable(blank ${DOODLE_PROJECT_ROOT_DIR}/blank.c)
+
+    message(STATUS "Linking doodle library")
+    target_link_libraries(blank PRIVATE doodle)
+endfunction()
+
+
