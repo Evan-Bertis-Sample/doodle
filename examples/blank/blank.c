6ec25c48a7baa645687e5414bd31026c3d68bc8b
evan-bertis-sample <esample21@gmail.com>
2024-12-18 16:08:56 -0500

    Hell yeah, actually linking with doodle

--- examples/blank/blank.c
+++ examples/blank/blank.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
+#include <doodle_math.h>
+
 int main() {
+    doodle_vec4_t vec = doodle_vec4_make(1.0f, 2.0f, 3.0f, 4.0f);
     printf("Hello, World!\n");
+
+    printf("vec: %f %f %f %f\n", vec.x, vec.y, vec.z, vec.w);
     return 0;
 }
