901e3a8714e613c05a57eb34b41888b8bc179fee
evan-bertis-sample <esample21@gmail.com>
2024-12-24 18:04:29 -0500

    Update pio doodle build script

--- doodle/platforms/pio/doodle.py
+++ doodle/platforms/pio/doodle.py
@@ -2,13 +2,16 @@
 
 import os
 import shutil
+import subprocess
 
-MAIN_FILE = "pio_main.c"
+MAIN_FILE = os.path.join(os.path.dirname(__file__), "pio_main.c")
 
-def get_pio_project_dir(output_dir : str):
+
+def get_pio_project_dir(output_dir: str):
     return os.path.join(output_dir, "pio_project")
 
-def build(project_path : str, project_name : str, output_dir : str):
+
+def build(project_path: str, project_name: str, output_dir: str):
     # literally copy-paste doodle into pio
     # this is a placeholder
     print(f"Building project {project_name} at {project_path} using platformIO")
@@ -20,16 +23,23 @@ def build(project_path : str, project_name : str, output_dir : str):
     if not os.path.exists(platformio_ini):
         print("Error: No platformio.ini file found in the project directory")
         return
-    
 
     # create the project directory
     pio_project_dir = get_pio_project_dir(output_dir)
     os.makedirs(pio_project_dir, exist_ok=True)
 
+    # erase everything in the project directory to make sure it is clean
+    for root, dirs, files in os.walk(pio_project_dir):
+        for file in files:
+            os.remove(os.path.join(root, file))
+
+        for dir in dirs:
+            shutil.rmtree(os.path.join(root, dir))
+
+
     # copy the platformio.ini file to the project directory
     pio_platformio_ini = os.path.join(pio_project_dir, "platformio.ini")
     shutil.copyfile(platformio_ini, pio_platformio_ini)
-
 
     # create all the necessary directories in the project directory
     os.makedirs(os.path.join(pio_project_dir, "src"), exist_ok=True)
@@ -42,41 +52,37 @@ def build(project_path : str, project_name : str, output_dir : str):
     lib_dir = os.path.join(project_path, "lib")
 
     if os.path.exists(src_dir):
-        shutil.copytree(src_dir, os.path.join(pio_project_dir, "src"), dirs_exist_ok=True)
-    
+        shutil.copytree(
+            src_dir, os.path.join(pio_project_dir, "src"), dirs_exist_ok=True
+        )
+
     if os.path.exists(include_dir):
-        shutil.copytree(include_dir, os.path.join(pio_project_dir, "include"), dirs_exist_ok=True)
+        shutil.copytree(
+            include_dir, os.path.join(pio_project_dir, "include"), dirs_exist_ok=True
+        )
 
     if os.path.exists(lib_dir):
-        shutil.copytree(lib_dir, os.path.join(pio_project_dir, "lib"), dirs_exist_ok=True)
-
-    # now for all of the remaining files in the project directory, it should be safe to just copy them into source
-    for root, dirs, files in os.walk(project_path):
-        for file in files:
-            if file == "platformio.ini":
-                continue
-
-            # copy the file
-            shutil.copyfile(os.path.join(root, file), os.path.join(pio_project_dir, "src", file))
-
-        for dir in dirs:
-            if dir == "src" or dir == "include" or dir == "lib":
-                continue
-
-            # copy the directory
-            shutil.copytree(os.path.join(root, dir), os.path.join(pio_project_dir, "src", dir), dirs_exist_ok=True)
-
+        shutil.copytree(
+            lib_dir, os.path.join(pio_project_dir, "lib"), dirs_exist_ok=True
+        )
+    
     # now copy the main file to the src directory
     main_file = os.path.join(pio_project_dir, "src", "main.c")
     shutil.copyfile(MAIN_FILE, main_file)
-    
+
     # now run the platformio build command
-    os.system(f"platformio run -d {pio_project_dir}")
+    # change the directory to the project directory
+    command = "pio run"
+
+    old_dir = os.getcwd()
+    os.chdir(pio_project_dir)
+    os.system(command)
+    os.chdir(old_dir)
 
     print("Build complete")
 
 
-def run(project_path : str, project_name : str, output_dir : str):
+def run(project_path: str, project_name: str, output_dir: str):
     # literally copy-paste doodle into pio
     # this is a placeholder
     print(f"Running project {project_name} at {project_path} using platformIO")
